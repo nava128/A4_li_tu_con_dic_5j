da11916c5c685e41872d77da04e8500373f2b03e
nava128 <96544099+nava128@users.noreply.github.com>
2024-09-09 10:12:37 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,18 @@
+
+print("Ejemplo de listas")
+arcoiris=["verde","azul","morado"]
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elemntos que contiene la lista arcoiris ", longitud)
+print(f"Elemntos que contiene la lista arcoiris v2 {longitud}")
+print("accediendo a un elemento de la lista")
+print(arcoiris[1])
+print(f"elemento en la posicion 0 es {arcoiris[0]}")
+print(f"El primer color del arcoiris es:",arcoiris[0])
+print("agregar a un elemento de la lista")
+print(arcoiris)
+arcoiris.append("naranja")
+print(arcoiris)
+print("Listar los elementos de la lista ciclo for")
+for nava in arcoiris:
+    print(nava)
---
+++ mi_tuplas.py
@@ -0,0 +1,8 @@
+print("Ejemplos de tuplas")
+canciones=("Te amo","El NoNoa","amor eterno")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "La puerta negra"
+x = tuple(y)
+print(x)
