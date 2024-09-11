d77500f3ccf3fff8ae5e72a485866117e7045a2a
nava128 <96544099+nava128@users.noreply.github.com>
2024-09-11 07:37:25 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo conjuntos set")
+tipodeportes={"futbol","Tenis","Basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+for nava in tipodeportes:
+    print(nava)
